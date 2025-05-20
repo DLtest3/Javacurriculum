3562556f567d6eb310192725dddc4623706e6081
driveline_3 <driveline_3@DESKTOP-229EBC3>
2025-05-20 22:06:06 +0900

    メッセージ

--- HelloWorld_InoueHiromu/src/sample/Sample.java
+++ HelloWorld_InoueHiromu/src/sample/Sample.java
@@ -3,7 +3,10 @@ package sample;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		System.out.println("今後、課題提出する際は");
+		System.out.println("HelloWorld_{あなたの名前}");
+		System.out.println("の中にパッケージやクラスを作って");
+		System.out.println("それをプッシュすることで提出してください");
 	}
 
 }
